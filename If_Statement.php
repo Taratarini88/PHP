a5fe7554a8c3e803d697d263a43d74880ca6a379
Taratarini88 <89439461+Taratarini88@users.noreply.github.com>
2023-10-06 18:56:44 +0530

    Update If_Statement.php

--- If_Statement.php
+++ If_Statement.php
@@ -1,4 +1,62 @@
 <?php
+
+$a = 3;
+$b = 10;
+
+if($a < $b){
+    echo "A is Smaller";
+}
+?>
+<?php
+
+$a = 30;
+$b = 10;
+
+if($a < $b){
+    echo "A is Smaller";
+}
+
+echo "Here is other statement";
+
+?>
+
+<?php
+
+$a = 30;
+$b = 30;
+
+if($a == $b){
+    echo "A is Smaller";
+}
+
+echo "Here is other statement";
+?>
+
+<?php
+
+$a = 30;
+$b = "30";
+
+if($a == $b){
+    echo "A is Smaller<br>";
+}
+
+echo "Here is other statement";
+?>
+<?php
+
+$a = 30;
+$b = "30";
+
+if($a === $b){
+    echo "A is Smaller<br>";
+}
+
+echo "Here is other statement";
+
+?>
+<?php
+
 $a = 30;
 $b = "30";
 
@@ -7,3 +65,17 @@ if($a == $b):
 endif;
 
 echo "Here is other statement";
+
+?>
+<?php
+
+$a = 30;
+$b = "30";
+
+if($a == $b):
+    echo "A is Smaller<br>";
+    echo "A is Smaller<br>";
+endif;
+
+echo "Here is other statement";
+?>
